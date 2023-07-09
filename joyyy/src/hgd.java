097a2786b5ca3c939ab51578cd6a47637bc12912
priyannnnnn <120243250+priyannnnnn@users.noreply.github.com>
2023-07-09 22:35:07 +0700

    Add files via upload

---
+++ joyyy/src/hgd.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class hgd {
+    public static void main(String[] args) {
+        // ArrayList<Integer> arr =new ArrayList<>();
+        // arr.add(3);
+        // arr.add(4);
+        // arr.add(5);
+        // arr.add(0, 1);
+        // arr.add(1, 2);
+
+        // ArrayList<Integer> ar2= new ArrayList<>();
+        // ar2.add(9, null);
+        // ar2.addAll(0, ar2);
+        // System.out.println(arr);
+        System.out.println("hhdgg");
+
+        // remove 
+        ArrayList<String> arr1= new ArrayList<>();
+        arr1.add("hello");
+        arr1.add("world");
+        arr1.add("my");
+        arr1.add("world");
+        arr1.remove("world");
+        System.out.println(arr1);
+    }
+    
+}
---
+++ joyyy/src/vfd.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class vfd {
+    public static void main(String[] args) {
+        // ArrayList<Integer> arr1= new ArrayList<Integer>();
+        // ArrayList<String> arr2= new ArrayList<String>();
+        // ArrayList<Character> arr3= new ArrayList<Character>();
+
+    }};
